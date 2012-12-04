6113598559439107eb38389139c12a3605373103
Patrick Konstandin <support@flatfiler.de>
2012-12-04 21:50:07 +0100

    fix time shift releases in rss feed
    Signed-off-by: Patrick Konstandin <support@flatfiler.de>

--- acp/core/pages.edit_rss.php
+++ acp/core/pages.edit_rss.php
@@ -5,42 +5,52 @@ require("core/access.php");
 
 $dbh = new PDO("sqlite:".CONTENT_DB);
 
-
-
 if($_REQUEST[delete] != "") {
 	$delete = (int) $_REQUEST[delete];
 	$sql = "DELETE FROM fc_feeds WHERE feed_id = $delete";
 	$cnt_changes = $dbh->exec($sql);
 }
 
-
-
-
 $sql = "SELECT * FROM fc_feeds ORDER BY feed_time DESC";
 
 if($dbh) {
    foreach($dbh->query($sql) as $row) {
-     $result[] = $row;
+     $rssItems[] = $row;
    }
 }
 
 
-$cnt_result = count($result);
+$cnt_rssItems = count($rssItems);
 
+$ts_now = time();
 
-for($i=0;$i<$cnt_result;$i++) {
+for($i=0;$i<$cnt_rssItems;$i++) {
 
-	$feed_time = date("d.m.Y H:i:s",$result[$i]['feed_time']);
-	$feed_id = $result[$i]['feed_id'];
-	$feed_title = $result[$i]['feed_title'];
-	$feed_text = $result[$i]['feed_text'];
-	$feed_url = $result[$i]['feed_url'];
+	$ts_feed_time = $rssItems[$i]['feed_time'];
+	$feed_time = date("d.m.Y H:i:s",$ts_feed_time);
+	$feed_id = $rssItems[$i]['feed_id'];
+	$feed_title = $rssItems[$i]['feed_title'];
+	$feed_text = $rssItems[$i]['feed_text'];
+	$feed_url = $rssItems[$i]['feed_url'];
 	
+	$ts_release = $ts_feed_time + (60*60); // 1 hour
+	$ts_diff = $ts_release-$ts_now;
 	
-	echo'<div class="modul_list_items">' . $feed_time;
+	$min = floor($ts_diff/60);
+	$sek = $ts_diff - ($min * 60);
+	
+	$ts_diff_string = " | Release in: $min:$sek ";
+	
+	if($ts_diff <= 0) {
+		$ts_diff = 0;
+		$ts_diff_string = "";
+	}
+	
+	echo'<div class="modul_list_items">';
+	echo"<h4>$feed_time $ts_diff_string</h4>";
 	echo"<h3>$feed_title</h3>";
 	echo"$feed_text";
-	echo"<a href='$feed_url' target='_blank'>$feed_url</a>";
+	echo"<p><a href='$feed_url' target='_blank'>$feed_url</a></p>";
 	echo"<div class='formfooter'>";
 	echo"<a class='btn btn-danger' href='$_SERVER[PHP_SELF]?tn=pages&sub=rss&delete=$feed_id' onclick=\"return confirm('$lang[confirm_delete_data]')\">$lang[delete]</a>";
 	echo"</div>";
@@ -52,31 +62,11 @@ for($i=0;$i<$cnt_result;$i++) {
 
 
 
-if($cnt_result < 1) {
+if($cnt_rssItems < 1) {
 	echo"<div class='alert alert-error'>";
 	echo"<p>No entries</p>";
 	echo"</div>";
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- rss.php
+++ rss.php
@@ -5,42 +5,39 @@ require('config.php');
 echo "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>";
 echo "<rss version=\"2.0\">";
 echo "<channel>";
-echo "<title>Nachrichten von $_SERVER[SERVER_NAME]</title>";
+echo "<title>RSS | $_SERVER[SERVER_NAME]</title>";
 echo "<link>http://$_SERVER[SERVER_NAME]</link>";
-echo "<description>Newsfeed von $_SERVER[SERVER_NAME]</description>";
+echo "<description>Newsfeed $_SERVER[SERVER_NAME]</description>";
 echo "<language>de-de</language>";
 
-
+$ts_now = time();
+$ts_min_release = $ts_now - (60*60);
 
 $dbh = new PDO("sqlite:$fc_db_content");
-$sql = "SELECT * FROM fc_feeds ORDER BY feed_time DESC";
+$sql = "SELECT * FROM fc_feeds WHERE feed_time > $ts_min_release ORDER BY feed_time DESC";
 
    foreach ($dbh->query($sql) as $row) {
-     $result[] = $row;
+     $rssItems[] = $row;
    }
 
-$cnt_result = count($result);
+$cnt_rssItems = count($rssItems);
 
 
-for($i=0;$i<$cnt_result;$i++) {
+for($i=0;$i<$cnt_rssItems;$i++) {
 
-$feed_time = date("d.m.Y H:i:s",$result[$i]['feed_time']);
-//$feed_time = $result[$i]['feed_time'];
-
-$feed_id = $result[$i]['feed_id'];
-$feed_title = $result[$i]['feed_title'];
-$feed_text = $result[$i]['feed_text'];
-$feed_url = $result[$i]['feed_url'];
-
-echo "	<item>
-			<title>$feed_title</title>
-			<description>$feed_text</description>
-			<link>$feed_url</link>
-			<pubDate>$feed_time</pubDate>
-		</item>\n";
-
-
-} // eo $i
+	$feed_time = date("d.m.Y H:i:s",$rssItems[$i]['feed_time']);
+	$feed_id = $rssItems[$i]['feed_id'];
+	$feed_title = $rssItems[$i]['feed_title'];
+	$feed_text = $rssItems[$i]['feed_text'];
+	$feed_url = $rssItems[$i]['feed_url'];
+	
+	echo "<item>
+					<title>$feed_title</title>
+					<description>$feed_text</description>
+					<link>$feed_url</link>
+					<pubDate>$feed_time</pubDate>
+				</item>\n";
+}
 
 echo "</channel>";
 echo "</rss>";
