5393387965c3f3e410eb93dae6c6a773dc932a22
Patrick Konstandin <support@flatfiler.de>
2013-06-25 15:24:34 +0200

    add new column - page_modul_query
    Signed-off-by: Patrick Konstandin <support@flatfiler.de>

--- install/contents/fc_pages_cache.php
+++ install/contents/fc_pages_cache.php
@@ -33,6 +33,7 @@ $cols = array(
 	"page_template" => 'VARCHAR',
 	"page_template_layout" => 'VARCHAR',
 	"page_modul" => 'VARCHAR',
+	"page_modul_query" => 'VARCHAR',
 	"page_authorized_users" => 'VARCHAR',
 	"page_version" => 'INTEGER',
 	"page_version_date" => 'VARCHAR',
