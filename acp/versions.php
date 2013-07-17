d747e0b6e08d21bb0b3aaed2c0842666a7a73ea3
Patrick Konstandin <support@flatfiler.de>
2013-07-17 11:42:23 +0200

    increase version info
    Signed-off-by: Patrick Konstandin <support@flatfiler.de>

--- acp/versions.php
+++ acp/versions.php
@@ -1,7 +1,7 @@
 <?php
 
-$fc_version_date = '2013-01-30';
-$fc_version_name = 'Release Candidate 2';
-$fc_version_build = '36';
+$fc_version_date = '2013-06-29';
+$fc_version_name = 'Release Candidate 3';
+$fc_version_build = '39';
 
 ?>
