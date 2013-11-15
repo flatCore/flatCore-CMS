9fa959969c97e49a644d381df19169a1e84b4a5c
Patrick Konstandin <support@flatfiler.de>
2013-11-15 10:39:44 +0100

    login functions merged
    Signed-off-by: Patrick Konstandin <support@flatfiler.de>

--- acp/index.php
+++ acp/index.php
@@ -1,62 +1,19 @@
 <?php
 session_start();
 error_reporting(0);
-require("../config.php");
+require('../config.php');
+require('../core/functions/func_userdata.php');
 
 $_SESSION['lang'] = "de";
 require("../lib/lang/$_SESSION[lang]/acp/dict.php");
 
-if($_POST[check] == "Login") {
+if($_POST['check'] == "Login") {
 
-	$login_name = strip_tags($_POST[login_name]);
-	$login_psw  = strip_tags($_POST[login_psw]);
+	$login_name = strip_tags($_POST['login_name']);
+	$login_psw  = strip_tags($_POST['login_psw']);
 	
+	fc_user_login($login_name,$login_psw,$acp=TRUE);
 	
-	// connect to database
-	$dbh = new PDO("sqlite:../$fc_db_user");
-	$sql = "SELECT * FROM fc_user	WHERE user_nick = :login_name AND user_verified = 'verified'";
-	$sth = $dbh->prepare($sql);
-	$sth->bindValue(':login_name', "$login_name", PDO::PARAM_STR);
-	$sth->execute();
-	
-	$result = $sth->fetch(PDO::FETCH_ASSOC);
-	
-	$dbh = null;
-	
-	$login_hash  = "$login_psw"."$login_name";
-	
-	if(md5($login_hash) == "$result[user_psw]") {
-	
-		// correct login - write data in session
-		$_SESSION[user_id] = "$result[user_id]";
-		$_SESSION[user_nick] = "$result[user_nick]";
-		$_SESSION[user_mail] = "$result[user_mail]";
-		$_SESSION[user_class] = "$result[user_class]";
-		$_SESSION[user_psw] = "$result[user_psw]";
-		$_SESSION[user_firstname] = "$result[user_firstname]";
-		$_SESSION[user_lastname] = "$result[user_lastname]";
-	
-		$arr_drm = explode("|", $result[user_drm]);
-	
-		if($arr_drm[0] == "drm_acp_pages")	{  $_SESSION[acp_pages] = "allowed";  }
-		if($arr_drm[1] == "drm_acp_files")	{  $_SESSION[acp_files] = "allowed";  }
-		if($arr_drm[2] == "drm_acp_user")	{  $_SESSION[acp_user] = "allowed";  }
-		if($arr_drm[3] == "drm_acp_system")	{  $_SESSION[acp_system] = "allowed";  }
-		if($arr_drm[4] == "drm_acp_editpages")	{  $_SESSION[acp_editpages] = "allowed";  }
-		if($arr_drm[5] == "drm_acp_editownpages")	{  $_SESSION[acp_editownpages] = "allowed";  }
-		if($arr_drm[6] == "drm_moderator")	{  $_SESSION[drm_moderator] = "allowed";  }
-		if($arr_drm[7] == "drm_can_publish")	{  $_SESSION[drm_can_publish] = "true";  }
-	
-		if($_SESSION[user_class] == "administrator"){
-			//goto acp
-			header("location:acp.php");
-		}
-	
-	
-	} else {
-		session_destroy();
-	}
-
 }
 
 ?>
@@ -64,7 +21,7 @@ if($_POST[check] == "Login") {
 <!DOCTYPE html>
 <html>
 <head>
-<title>flatCore ACP:Login <?php echo"@ $_SERVER[SERVER_NAME]"; ?></title>
+<title>Login <?php echo $_SERVER['SERVER_NAME']; ?></title>
 <link rel="stylesheet" href="../lib/css/bootstrap.css" type="text/css" media="screen, projection">
 
 <style type="text/css">
--- core/functions/func_userdata.php
+++ core/functions/func_userdata.php
@@ -142,9 +142,9 @@ return($result);
 
 
 
-/*
-Generate random Password
-*/
+/**
+ * Generate random Password
+ */
 
 
 function randpsw() {
@@ -155,14 +155,72 @@ function randpsw() {
 	$random_s = '';
 	$cnt_chars = strlen($chars);
 	for($i=0;$i<$length;$i++) {
-	$random_s .= $chars[mt_rand(0, $cnt_chars - 1)];
+		$random_s .= $chars[mt_rand(0, $cnt_chars - 1)];
 	}
 	return $random_s;
 }
 
 
 
+/**
+ * user login
+ */
+ 
+function fc_user_login($user,$psw,$acp = NULL) {
 
+	global $fc_db_user;
+	unset($result);
+	
+	if($acp == TRUE) {
+		$fc_db_user = '../'.$fc_db_user;
+	}
+	
+	$login_name = strip_tags($user);
+	$login_psw  = strip_tags($psw);
+	$login_hash  = md5($login_psw.$login_name);
+
+	
+	$dbh = new PDO("sqlite:$fc_db_user");
+	$sql = "SELECT * FROM fc_user	WHERE user_nick = :login_name AND user_psw = :login_hash AND user_verified = 'verified'";
+	$sth = $dbh->prepare($sql);
+	$sth->bindValue(':login_name', "$login_name", PDO::PARAM_STR);
+	$sth->bindValue(':login_hash', "$login_hash", PDO::PARAM_STR);
+	$sth->execute();
+	$result = $sth->fetch(PDO::FETCH_ASSOC);
+	$dbh = null;
+	
+	$cnt_result = count($result);
+	
+	if($cnt_result>1) {
+		$_SESSION['user_id'] = $result['user_id'];
+		$_SESSION['user_nick'] = $result['user_nick'];
+		$_SESSION['user_mail'] = $result['user_mail'];
+		$_SESSION['user_class'] = $result['user_class'];
+		$_SESSION['user_psw'] = $result['user_psw'];
+		$_SESSION['user_firstname'] = $result['user_firstname'];
+		$_SESSION['user_lastname'] = $result['user_lastname'];
+		$_SESSION['user_hash'] = md5($result['user_nick']);
+		
+		$arr_drm = explode("|", $result['user_drm']);
+
+		if($arr_drm[0] == "drm_acp_pages")	{  $_SESSION['acp_pages'] = "allowed";  }
+		if($arr_drm[1] == "drm_acp_files")	{  $_SESSION['acp_files'] = "allowed";  }
+		if($arr_drm[2] == "drm_acp_user")	{  $_SESSION['acp_user'] = "allowed";  }
+		if($arr_drm[3] == "drm_acp_system")	{  $_SESSION['acp_system'] = "allowed";  }
+		if($arr_drm[4] == "drm_acp_editpages")	{  $_SESSION['acp_editpages'] = "allowed";  }
+		if($arr_drm[5] == "drm_acp_editownpages")	{  $_SESSION['acp_editownpages'] = "allowed";  }
+		if($arr_drm[6] == "drm_moderator")	{  $_SESSION['drm_moderator'] = "allowed";  }
+		if($arr_drm[7] == "drm_can_publish")	{  $_SESSION['drm_can_publish'] = "true";  }
+		
+		if(($acp == TRUE) AND ($_SESSION['user_class'] == "administrator")) {
+			header("location:acp.php");
+		}
+		
+	} else {
+		session_destroy();
+	}
+
+}
 
 
 
