c3c9ee4c4e838cb63b9172d54dc1c543b626d01e
Patrick <support@flatcore.de>
2014-07-30 15:15:32 +0200

    increase the version data
    Signed-off-by: Patrick <support@flatcore.de>

--- acp/versions.php
+++ acp/versions.php
@@ -1,7 +1,7 @@
 <?php
 
-$fc_version_date = '2014-07-24';
-$fc_version_name = 'Release Candidate 4';
-$fc_version_build = '43';
+$fc_version_date = '2014-07-30';
+$fc_version_name = '1.0';
+$fc_version_build = '44';
 
 ?>
