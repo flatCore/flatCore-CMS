3b56e58ad13aedbaa6578c515f3775a83c57defb
Patrick <support@flatcore.de>
2014-09-05 11:52:52 +0200

    cleanup
    Signed-off-by: Patrick <support@flatcore.de>

--- acp/core/inc.moduls.php
+++ acp/core/inc.moduls.php
@@ -3,26 +3,12 @@
 //prohibit unauthorized access
 require("core/access.php");
 
-
-
-
-
-
-
-
-//include("$subinc.php");
-
-
-if($_GET[a] == "") {
+if(!isset($_GET['a'])) {
 	include("mods.list.php");
 } else {
-
 	unset($mod);
 	include("../modules/$sub/info.inc.php");
 	include("../modules/$sub/backend/$a.php");
-
 }
-
-
 
 ?>
