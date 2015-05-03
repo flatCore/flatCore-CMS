80ef53b8361dacf96f6ccb7418b56ff39fd117f7
Patrick <support@flatcore.de>
2015-05-03 19:39:34 +0200

    Stable Version 1.3

--- acp/versions.php
+++ acp/versions.php
@@ -1,7 +1,7 @@
 <?php
 
-$fc_version_date = '2015-04-24';
-$fc_version_name = '1.2.1';
-$fc_version_build = '49';
+$fc_version_date = '2015-05-03';
+$fc_version_name = '1.3';
+$fc_version_build = '50';
 
 ?>
