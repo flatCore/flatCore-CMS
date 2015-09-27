807f4a777d2ff250d2b9e41f944aed25d2b0ad8d
Patrick <support@flatcore.de>
2015-09-27 16:01:01 +0200

    now you can use all snippets in Templates -> {$fc_snippet_xxx}

--- core/functions/func_textlib.php
+++ core/functions/func_textlib.php
@@ -5,21 +5,11 @@
  * Source: lang/$lang/msg/$file.txt
  */
 
-function print_msg($file,$l) {
-
-	
+function print_msg($file,$l) {	
 	$text = file_get_contents("lib/lang/$l/msg/$file");
 	$text = nl2br($text);
-	
 	return($text);
-
 }
-
-
-
-
-
-
 
 
 /**
@@ -31,38 +21,27 @@ function get_textlib($textlib_name,$textlib_lang='de') {
 
 	global $fc_db_content;
 	
-	
 	try {
 		$dbh = new PDO("sqlite:$fc_db_content");
-	
 		$text = $dbh->quote($text);
-	
 		$sql = 'SELECT * FROM fc_textlib WHERE textlib_name = :textlib_name AND textlib_lang = :textlib_lang ';
-
 		$sth = $dbh->prepare($sql);
 		$sth->bindParam(':textlib_name', $textlib_name, PDO::PARAM_STR);
 		$sth->bindParam(':textlib_lang', $textlib_lang, PDO::PARAM_STR);
-	
 		$sth->execute();
 		$textlibData = $sth->fetch(PDO::FETCH_ASSOC);
-	
 		$dbh = null;
-	
-	
+		
 		foreach($textlibData as $k => $v) {
 	   		$$k = stripslashes($v);
 		}
-	
-	
-		return $textlib_content;
-	
+
+		return $textlib_content;	
 	}
 	
 	catch (PDOException $e) {
 		echo 'Error: ' . $e->getMessage();
 	}
-
-
 }
 
 
@@ -74,10 +53,8 @@ function get_textlib_by_fn($fn) {
 	$dbh = new PDO("sqlite:$fc_db_content");
 	$fn = $dbh->quote($fn);
 	$sql = "SELECT * FROM fc_textlib WHERE textlib_name = $fn ";
-
 	$result = $dbh->query($sql);
 	$result= $result->fetch(PDO::FETCH_ASSOC);
-
 	$dbh = null;
 
 	foreach($result as $k => $v) {
@@ -90,18 +67,16 @@ function get_textlib_by_fn($fn) {
 
 
 function get_all_textlibs() {
+	
 	global $fc_db_content;
+	
 	$dbh = new PDO("sqlite:$fc_db_content");
-	$sql = "SELECT textlib_name, textlib_content FROM fc_textlib WHERE textlib_id > '7'";
-
+	$sql = "SELECT textlib_name, textlib_content FROM fc_textlib";
 	$result = $dbh->query($sql);
 	$result= $result->fetchAll(PDO::FETCH_ASSOC);
-  
-
 	$dbh = null;
 	
 	return $result;
-	
 }
 
 
