833035f23a35fd43c48518e9ced27dc62dde9e33
Patrick <support@flatcore.de>
2016-03-08 15:28:29 +0100

    point Help-Link to the new URLs

--- acp/core/livebox.php
+++ acp/core/livebox.php
@@ -1,11 +1,17 @@
 <div id="liveBox">
-	<p>
-		<a class="fancybox-docs" href="http://www.flatcore.de/support/"><span class="glyphicon glyphicon-question-sign"></span> </i> <?php echo $lang['show_help']; ?></a><br>
-		<a href='../'><span class="glyphicon glyphicon-home"></span>  <?php echo $lang['back_to_page']; ?></a>
-	</p>
-	<p>
-		<a href='../index.php?goto=logout'><span class="glyphicon glyphicon-off"></span> <?php echo $lang['logout']; ?></a>
-	</p>
-
+	<?php
+		
+		$helpURL = 'http://docs.flatcore.org/en/';
+		
+		if($languagePack == 'de') {
+			$helpURL = 'http://docs.flatcore.org/de/';	
+		}
+		
+		echo '<a class="fancybox-docs" href="'.$helpURL.'"><span class="glyphicon glyphicon-question-sign"></span> '.$lang['show_help'].'</a>';
+		
+	?>		
+		
+	<a href='../'><span class="glyphicon glyphicon-home"></span>  <?php echo $lang['back_to_page']; ?></a>
+	<hr>
+	<a href='../index.php?goto=logout'><span class="glyphicon glyphicon-off"></span> <?php echo $lang['logout']; ?></a>
 </div>
-
