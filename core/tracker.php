e111ea469c94ca1b924ba348e6f4bf03e1313ff1
Patrick <support@flatcore.de>
2017-04-19 09:26:17 +0200

    Use Prepared Statements

--- core/tracker.php
+++ core/tracker.php
@@ -13,14 +13,26 @@ if($page_contents['page_sort'] == "portal") {
 	$hits_page_id = "portal_$languagePack";
 }
 
-$sql = "SELECT counter FROM hits WHERE page_id='$hits_page_id'";
-$result = $dbh->query($sql)->fetchAll();
+$sql = 'SELECT counter FROM hits WHERE page_id = :hits_page_id';
+$sth = $dbh->prepare($sql);
+$sth->bindParam(':hits_page_id', $hits_page_id, PDO::PARAM_STR);
+$sth->execute();
+$counter = $sth->fetchAll(PDO::FETCH_ASSOC);
 
-if(sizeof($result) != 0) {
-	$set_counter = $result[0]['counter'] + 1;
-	$dbh->exec("UPDATE hits SET counter=$set_counter WHERE page_id='$hits_page_id'");
+if(sizeof($counter) != 0) {
+	$set_counter = $counter[0]['counter'] + 1;
+	$sql = 'UPDATE hits SET counter = :set_counter WHERE page_id = :hits_page_id';
+	$sth = $dbh->prepare($sql);
+	$sth->bindParam(':set_counter', $set_counter, PDO::PARAM_STR);
+	$sth->bindParam(':hits_page_id', $hits_page_id, PDO::PARAM_STR);
+	$sth->execute();
+	
 } else {
-	$dbh->exec("INSERT INTO hits (page_id, counter) VALUES ('$hits_page_id', 1)");
+	$sql = 'INSERT INTO hits (page_id, counter) VALUES (:hits_page_id, 1)';
+	$sth = $dbh->prepare($sql);
+	$sth->bindParam(':hits_page_id', $hits_page_id, PDO::PARAM_STR);
+	$sth->execute();
+	
 }
 
 $dbh = null;
