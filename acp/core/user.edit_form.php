80247a2a4c9e49630b655d99af9e8afdf446422c
Patrick <support@flatcore.de>
2017-08-21 23:18:26 +0200

    add missing class if WYSIWYG isn’t active

--- acp/core/user.edit_form.php
+++ acp/core/user.edit_form.php
@@ -228,7 +228,7 @@ echo'<div class="tab-pane fade" id="custom">';
 			echo tpl_form_control_group('',$label,"<textarea class='form-control' rows='6' name='$custom_fields[$i]'>" . ${$custom_fields[$i]} . "</textarea>");
 		}	elseif(substr($custom_fields[$i],0,14) == "custom_wysiwyg") {
 			$label = substr($custom_fields[$i],15);
-			echo tpl_form_control_group('',$label,"<textarea class='mceEditor_small' name='$custom_fields[$i]'>" . ${$custom_fields[$i]} . "</textarea>");
+			echo tpl_form_control_group('',$label,"<textarea class='mceEditor_small form-control' name='$custom_fields[$i]'>" . ${$custom_fields[$i]} . "</textarea>");
 		}		
 	}
 
