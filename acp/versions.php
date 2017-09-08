9313d37f2258ee5411be61aafb8959b627eca4e6
Patrick <support@flatcore.de>
2017-09-08 12:34:43 +0200

    increase version info (build 83)

--- acp/versions.php
+++ acp/versions.php
@@ -1,5 +1,5 @@
 <?php
-$fc_version_date = '2017-09-04';
-$fc_version_name = '1.4.7';
-$fc_version_build = '82';
+$fc_version_date = '2017-09-08';
+$fc_version_name = '1.4.7.1';
+$fc_version_build = '83';
 ?>
