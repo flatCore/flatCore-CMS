35fee6433b5c7ec1c593bd005b9c16ae0610f995
Patrick <support@flatcore.de>
2018-02-12 17:56:36 +0100

    TinyMCE 4.7.6

--- acp/versions.php
+++ acp/versions.php
@@ -1,5 +1,5 @@
 <?php
-$fc_version_date = '2018-02-09';
-$fc_version_name = '1.4.7.3';
-$fc_version_build = '86';
+$fc_version_date = '2018-02-12';
+$fc_version_name = '1.4.7.4';
+$fc_version_build = '87';
 ?>
