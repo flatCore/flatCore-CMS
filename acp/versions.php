6be2a5ad82a4064e0ca850045bd576d92b58db99
Patrick <support@flatcore.de>
2018-04-05 07:57:28 +0200

    increase version info (build 90)

--- acp/versions.php
+++ acp/versions.php
@@ -1,5 +1,5 @@
 <?php
-$fc_version_date = '2018-03-16';
-$fc_version_name = '1.4.7.5';
-$fc_version_build = '89';
+$fc_version_date = '2018-04-05';
+$fc_version_name = '1.4.7.6';
+$fc_version_build = '90';
 ?>
