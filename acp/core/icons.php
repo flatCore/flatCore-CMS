99c890330f0bdc1c8ef0f1cb78b212d08dc2d934
Patrick <support@flatcore.de>
2019-03-14 20:54:51 +0100

    add some icons

--- acp/core/icons.php
+++ acp/core/icons.php
@@ -4,7 +4,8 @@
  * icons from fontawesome
  */
 
-
+$icon['address_book'] = '<i class="fas fa-address-book"></i>';
+$icon['address_card'] = '<i class="far fa-address-card"></i>';
 $icon['angle_left']  = '<i class="far fa-angle-left"></i>';
 $icon['angle_right']  = '<i class="far fa-angle-right"></i>';
 $icon['angle_up']  = '<i class="far fa-angle-up"></i>';
@@ -12,29 +13,26 @@ $icon['angle_down']  = '<i class="far fa-angle-down"></i>';
 $icon['angle_double_right']  = '<i class="fas fa-angle-double-right"></i>';
 $icon['angle_double_left']  = '<i class="fas fa-angle-double-left"></i>';
 
-$icon['check'] = '<i class="far fa-check"></i>';
 $icon['ban'] = '<i class="far fa-ban"></i>';
 $icon['bars'] = '<i class="fas fa-bars"></i>';
+$icon['bookmark'] = '<i class="fas fa-bookmark"></i>';
 
+$icon['check'] = '<i class="far fa-check"></i>';
 $icon['caret_left'] = '<i class="fas fa-caret-left"></i>';
 $icon['caret_right'] = '<i class="fas fa-caret-right"></i>';
 $icon['circle'] = '<i class="fas fa-circle"></i>';
 $icon['clipboard'] = '<i class="fas fa-clipboard"></i>';
-$icon['cogs'] = '<i class="fas fa-cogs"></i>';
-
-$icon['edit'] = '<i class="fas fa-edit"></i>';
-$icon['copy']  = '<i class="fas fa-copy"></i>';
-$icon['clock']  = '<i class="far fa-clock"></i>';
-
-
-$icon['exclamation_triangle']  = '<i class="fas fa-exclamation-triangle"></i>';
-$icon['user_minus']  = '<i class="fas fa-user-minus"></i>';
-
-$icon['circle'] = '<i class="fas fa-circle"></i>';
 $icon['code'] = '<i class="fas fa-code"></i>';
 $icon['comments'] = '<i class="fas fa-comments"></i>';
 $icon['compress']  = '<i class="fas fa-compress"></i>';
-$icon['address_card'] = '<i class="far fa-address-card"></i>';
+$icon['copy']  = '<i class="fas fa-copy"></i>';
+$icon['clipboard_list']  = '<i class="fas fa-clipboard-list"></i>';
+$icon['clock']  = '<i class="far fa-clock"></i>';
+$icon['cogs'] = '<i class="fas fa-cogs"></i>';
+
+$icon['edit'] = '<i class="fas fa-edit"></i>';
+$icon['exclamation_triangle']  = '<i class="fas fa-exclamation-triangle"></i>';
+
 $icon['chart_bar'] = '<i class="fas fa-chart-bar"></i>';
 $icon['code'] = '<i class="far fa-code"></i>';
 $icon['cog'] = '<i class="far fa-cog"></i>';
@@ -49,13 +47,18 @@ $icon['folder_open'] = '<i class="fas fa-folder-open"></i>';
 $icon['home'] = '<i class="fas fa-home"></i>';
 
 $icon['info_circle'] = '<i class="far fa-info-circle"></i>';
+$icon['images'] = '<i class="fas fa-images"></i>';
 
+$icon['language'] = '<i class="fas fa-language"></i>';
 $icon['level_up_alt'] = '<i class="fas fa-level-up-alt"></i>';
 $icon['list'] = '<i class="far fa-list"></i>';
 $icon['lock'] = '<i class="far fa-lock"></i>';
 $icon['lock_open'] = '<i class="far fa-lock-open"></i>';
 $icon['long_arrow_alt_right'] = '<i class="fas fa-long-arrow-alt-right"></i>';
 
+$icon['map_marker_alt'] = '<i class="fas fa-map-marker-alt"></i>';
+
+$icon['paper_plane'] = '<i class="fas fa-paper-plane"></i>';
 $icon['paste'] = '<i class="fas fa-paste"></i>';
 $icon['plus'] = '<i class="fas fa-plus"></i>';
 
@@ -66,10 +69,16 @@ $icon['rss'] = '<i class="fas fa-rss"></i>';
 $icon['search'] = '<i class="fas fa-search"></i>';
 $icon['server'] = '<i class="fas fa-server"></i>';
 $icon['share'] = '<i class="far fa-share"></i>';
+$icon['shopping_basket'] = '<i class="fas fa-shopping-basket"></i>';
 $icon['sign_out_alt'] = '<i class="fas fa-sign-out-alt"></i>';
 $icon['sitemap'] = '<i class="fas fa-sitemap"></i>';
+$icon['star'] = '<i class="fas fa-star"></i>';
+$icon['star_outline'] = '<i class="far fa-star"></i>';
+$icon['store'] = '<i class="fas fa-store"></i>';
 $icon['sync_alt'] = '<i class="fas fa-sync-alt"></i>';
 
+$icon['tags'] = '<i class="fas fa-tags"></i>';
+$icon['tasks'] = '<i class="fas fa-tasks"></i>';
 $icon['trash_alt'] = '<i class="far fa-trash-alt"></i>';
 $icon['times_circle'] = '<i class="fas fa-times-circle"></i>';
 
@@ -81,11 +90,12 @@ $icon['user_cog'] = '<i class="fas fa-user-cog"></i>';
 $icon['user_edit'] = '<i class="fas fa-user-edit"></i>';
 $icon['user_friends'] = '<i class="fas fa-user-friends"></i>';
 $icon['user_lock'] = '<i class="fas fa-user-lock"></i>';
+$icon['user_minus']  = '<i class="fas fa-user-minus"></i>';
 $icon['user_plus'] = '<i class="fas fa-user-plus"></i>';
 $icon['user_slash'] = '<i class="fas fa-user-slash"></i>';
 $icon['user_plus'] = '<i class="fas fa-user-plus"></i>';
-
 $icon['upload'] = '<i class="fas fa-upload"></i>';
+
 $icon['wrench'] = '<i class="fas fa-wrench"></i>';
 
 
