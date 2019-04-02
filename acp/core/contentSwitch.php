28cc14cb7248bd948277914ed5c78ca054ecdb00
Patrick <support@flatcore.de>
2019-04-02 19:57:12 +0200

    improved - switch between database files

--- acp/core/contentSwitch.php
+++ acp/core/contentSwitch.php
@@ -1,5 +1,6 @@
 <?php
-require("core/access.php");
+	
+require 'core/access.php';
 
 /**
  * we can choose between different content files
@@ -27,14 +28,20 @@ if(isset($_SESSION['fc_db_content'])) {
 }
 
 
-$fc_content_switch = '<a href="#" class="tooltip_bottom" data-toggle="collapse" data-target="#contentSwitchContainer"><span class="glyphicon glyphicon-collapse-down"></span> '.$fc_db_content.'</a>';
+$fc_content_switch = '<button class="btn btn-dark btn-sm" data-target="#contentSwitchContainer" data-toggle="collapse">'.$icon['angle_down'].' '.$fc_db_content.'</button>';
 
-$fc_content_switch .= '<div id="contentSwitchContainer" class="collapse" style="height:auto;">';
-$fc_content_switch .= '<div>';
+$fc_content_switch .= '<div id="contentSwitchContainer" class="collapse">';
+$fc_content_switch .= '<div class="well well-sm">';
 
 $i=0;
 foreach($fc_content_files as $files) {
-	$fc_content_switch .= '<a class="btn btn-light btn-xs btn-block" href="acp.php?tn='.$_GET['tn'].'&amp;switchContent='.$i.'" title="'.$files['desc'].'">'.$files['file'].'</a>';
+	$btn_class = 'btn-outline-secondary';
+	
+	if($files['file'] == basename($fc_db_content)) {
+		$btn_class = 'btn-secondary';
+	}
+	
+	$fc_content_switch .= '<a class="btn btn-sm m-1 '.$btn_class.'" href="acp.php?tn='.$_GET['tn'].'&amp;switchContent='.$i.'" title="'.$files['desc'].'">'.$icon['database'].' '.$files['file'].'</a>';
 	$i++;
 }
 
