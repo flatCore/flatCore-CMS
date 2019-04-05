2b2d40dbc1f8cc70db01a2c9c4a6e0655dbb420a
Patrick <support@flatcore.de>
2019-04-05 09:25:18 +0200

    show addons in columns

--- acp/core/dashboard.addons.php
+++ acp/core/dashboard.addons.php
@@ -10,13 +10,17 @@ $cnt_mods = count($all_mods);
 if($cnt_mods > 0) {
 	echo '<hr class="shadow">';
 	
-	echo '<div class="row">';
+	echo '<div class="card-columns custom-columns">';
+	
 	for($i=0;$i<$cnt_mods;$i++) {
 		$modFolder = $all_mods[$i]['folder'];
 	
 		$mod_info_file = "../modules/$modFolder/info.inc.php";
 		
-		
+		$poster_img = '';
+		if(is_file("../modules/$modFolder/backend/poster.jpg")) {
+			$poster_img = '<a href="acp.php?tn=moduls&sub='.$modFolder.'&a=start"><img src="../modules/'.$modFolder.'/backend/poster.jpg" class="card-img-top"></a>';
+		}
 		
 		if(is_file("$mod_info_file")) {
 			
@@ -24,28 +28,19 @@ if($cnt_mods > 0) {
 			include $mod_info_file;
 			
 			$mod_id = 'id_'.clean_filename($mod['name']);
-			
-			echo '<div class="col-md m-1">';
-			
+						
 			echo '<div class="card">';
-			echo '<div class="card-header"><a data-toggle="collapse" href="#" data-target="#'.$mod_id.'">'.$icon['angle_down'].' '.$mod['name'].'</a> <span class="badge badge-dark float-right">v'.$mod['version'].'</span></div>';
-
-			echo '<div class="card-body text-center p-1 collapse" id="'.$mod_id.'">';
+			echo '<div class="card-header p-1"><strong>'.$mod['name'].'</strong> <span class="badge badge-dark float-right">v'.$mod['version'].'</span></div>';
+			echo $poster_img;
+						
+			echo '<div class="list-group list-group-flush">';
 			
 			foreach($modnav as $nav) {
-				echo '<a class="btn btn-dark btn-sm m-1" href="acp.php?tn=moduls&sub='.$modFolder.'&a='.$nav['file'].'">'.$nav['link'].'</a>';
+				echo '<a class="list-group-item list-group-item-ghost p-1 px-2" href="acp.php?tn=moduls&sub='.$modFolder.'&a='.$nav['file'].'">'.$nav['link'].'</a>';
 			}
 			
-			
-			echo '</div>';
-			echo '</div>';
-			
-			echo '</div>';
-			
-			if($i % 2 == 0 && $i > 0) {
-				echo '<div class="w-100"></div>';
-			}
-			
+			echo '</div>';			
+			echo '</div>';		
 			
 		}
 		
