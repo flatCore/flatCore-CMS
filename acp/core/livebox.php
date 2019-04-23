cc3373642e6738317fcca3d431b066a1acc6ea71
Patrick <support@flatcore.de>
2019-04-23 18:34:19 +0200

    new links to docs

--- acp/core/livebox.php
+++ acp/core/livebox.php
@@ -1,13 +1,13 @@
 <div class="sidebar-footer">
 	<?php
 		
-		$helpURL = 'https://flatcore.org/docs/en/';
+		$helpURL = 'https://flatcore.org/documentation/';
 		
 		if($languagePack == 'de') {
-			$helpURL = 'https://flatcore.org/docs/de/';	
+			$helpURL = 'https://flatcore.org/de/dokumentation/';	
 		}
 		
-		echo '<a class="fancybox-docs" href="'.$helpURL.'">'.$icon['question'].' '.$lang['show_help'].'</a>';
+		echo '<a target="_blank" href="'.$helpURL.'">'.$icon['question'].' '.$lang['show_help'].'</a>';
 		
 		echo '<a href="../">'.$icon['home'].' '.$lang['back_to_page'].'</a>';
 		echo '<a href="../index.php?goto=logout">'.$icon['sign_out_alt'].' '.$lang['logout'].'</a>';
