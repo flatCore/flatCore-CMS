fc698dba962ec13dd0cd999a7491b3b4e8444b2a
Patrick <support@flatcore.de>
2020-02-01 14:28:01 +0100

    increase version information (build 112)

--- acp/versions.php
+++ acp/versions.php
@@ -1,5 +1,5 @@
 <?php
-$fc_version_date = '2020-01-26';
-$fc_version_name = '1.5.5';
-$fc_version_build = '111';
+$fc_version_date = '2020-02-01';
+$fc_version_name = '1.5.6';
+$fc_version_build = '112';
 ?>
