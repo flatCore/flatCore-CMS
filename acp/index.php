01b98a4eac025c8baa0ad5a761d82f9d77500587
Patrick <support@flatcore.de>
2020-09-27 16:31:27 +0200

    [fix] check for database type

--- acp/index.php
+++ acp/index.php
@@ -1,7 +1,75 @@
 <?php
 session_start();
 error_reporting(E_ALL ^E_NOTICE);
+
+require '../lib/Medoo.php';
+use Medoo\Medoo;
+
 require '../config.php';
+if(is_file('../'.FC_CONTENT_DIR.'/config.php')) {
+	include '../'.FC_CONTENT_DIR.'/config.php';
+}
+
+
+if(is_file('../config_database.php')) {
+	include '../config_database.php';
+	$db_type = 'mysql';
+	
+	$database = new Medoo([
+
+		'database_type' => 'mysql',
+		'database_name' => "$database_name",
+		'server' => "$database_host",
+		'username' => "$database_user",
+		'password' => "$database_psw",
+	 
+		'charset' => 'utf8',
+		'port' => $database_port,
+	 
+		'prefix' => DB_PREFIX
+	]);
+	
+	$db_content = $database;
+	$db_user = $database;
+	$db_statistics = $database;	
+	
+	
+	
+} else {
+	$db_type = 'sqlite';
+	
+	if(isset($fc_content_files) && is_array($fc_content_files)) {
+		/* switch database file $fc_db_content */
+		include 'core/contentSwitch.php';
+	}
+	
+	
+	define("CONTENT_DB", "../$fc_db_content");
+	define("USER_DB", "../$fc_db_user");
+	define("STATS_DB", "../$fc_db_stats");	
+
+	$db_content = new Medoo([
+		'database_type' => 'sqlite',
+		'database_file' => CONTENT_DB
+	]);
+	
+	$db_user = new Medoo([
+		'database_type' => 'sqlite',
+		'database_file' => USER_DB
+	]);
+	
+	$db_statistics = new Medoo([
+		'database_type' => 'sqlite',
+		'database_file' => STATS_DB
+	]);	
+	
+}
+
+
+
+
+
+
 require '../core/functions/func_userdata.php';
 require '../lib/lang/'.$languagePack.'/dict-backend.php';
 
