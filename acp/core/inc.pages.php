9e5876e9e990d7f0526f5d37d32e75641f90a3c9
Patrick <support@flatcore.org>
2021-03-11 10:14:10 +0100

    fix - filter pages and snippets by labels (again)

--- acp/core/inc.pages.php
+++ acp/core/inc.pages.php
@@ -133,7 +133,7 @@ $checked_labels_array = explode('-', $_SESSION['checked_label_str']);
 for($i=0;$i<count($fc_labels);$i++) {
 	$label = $fc_labels[$i]['label_id'];
 	if(in_array($label, $checked_labels_array)) {
-		$set_label_filter .= "page_labels LIKE '%$label,%' OR page_labels LIKE '$label' OR ";
+		$set_label_filter .= "page_labels LIKE '%,$label,%' OR page_labels LIKE '%,$label' OR page_labels LIKE '$label,%' OR page_labels = '$label' OR ";
 	}
 }
 
