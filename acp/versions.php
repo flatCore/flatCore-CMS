a50f6ad283afe96eb3b131c15137ac680a012f67
Patrick <support@flatcore.org>
2021-04-07 11:54:30 +0200

    increase version information (build 156)

--- acp/versions.php
+++ acp/versions.php
@@ -1,5 +1,5 @@
 <?php
-$fc_version_date = '2021-03-26';
-$fc_version_name = '2.0.6';
-$fc_version_build = '155';
+$fc_version_date = '2021-04-07';
+$fc_version_name = '2.0.7';
+$fc_version_build = '156';
 ?>
