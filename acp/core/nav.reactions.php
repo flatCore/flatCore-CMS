564183b323461e0164d4769b3f27e2c6d32b26d3
Patrick <support@flatcore.org>
2021-04-29 15:51:34 +0200

    fix - show active page in sidebar

--- acp/core/nav.reactions.php
+++ acp/core/nav.reactions.php
@@ -8,9 +8,9 @@ if($sub == '') {
 
 echo '<ul class="nav">';
 
-echo '<li><a class="sidebar-nav '.($sub == "list" ? 'sidebar-nav-active' :'').'" href="acp.php?tn=reactions&sub=comments">'.$icon['comments'].' '.$lang['reactions_comments'].'</a></li>';
-echo '<li><a class="sidebar-nav '.($sub == "list" ? 'sidebar-nav-active' :'').'" href="acp.php?tn=reactions&sub=votings">'.$icon['thumbs_up'].' '.$lang['reactions_votings'].'</a></li>';
-echo '<li><a class="sidebar-nav '.($sub == "list" ? 'sidebar-nav-active' :'').'" href="acp.php?tn=reactions&sub=events">'.$icon['calendar_check'].' '.$lang['reactions_events'].'</a></li>';
+echo '<li><a class="sidebar-nav '.($sub == "comments" ? 'sidebar-nav-active' :'').'" href="acp.php?tn=reactions&sub=comments">'.$icon['comments'].' '.$lang['reactions_comments'].'</a></li>';
+echo '<li><a class="sidebar-nav '.($sub == "votings" ? 'sidebar-nav-active' :'').'" href="acp.php?tn=reactions&sub=votings">'.$icon['thumbs_up'].' '.$lang['reactions_votings'].'</a></li>';
+echo '<li><a class="sidebar-nav '.($sub == "events" ? 'sidebar-nav-active' :'').'" href="acp.php?tn=reactions&sub=events">'.$icon['calendar_check'].' '.$lang['reactions_events'].'</a></li>';
 
 echo '</ul>';
 
