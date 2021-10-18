c8fd0b5ba09f67292f0d00efcfee41a2650a7dc1
Patrick <pk@konstand.de>
2021-10-18 20:44:21 +0200

    Update versions.php

--- acp/versions.php
+++ acp/versions.php
@@ -1,5 +1,5 @@
 <?php
-$fc_version_date = '2021-10-13';
-$fc_version_name = '2.1.0';
-$fc_version_build = '176';
+$fc_version_date = '2021-10-18';
+$fc_version_name = '2.1.1';
+$fc_version_build = '177';
 ?>
