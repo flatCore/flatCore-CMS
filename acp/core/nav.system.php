9cf920a07502e7641628ff88f868c5609d80b7c4
Patrick <pk@konstand.de>
2022-04-08 12:05:03 +0200

    change icon

--- acp/core/nav.system.php
+++ acp/core/nav.system.php
@@ -14,7 +14,7 @@ echo '<li><a class="sidebar-nav '.($sub == "images" ? 'sidebar-nav-active' :'').
 echo '<li class="mt-2"><a class="sidebar-nav '.($sub == "labels" ? 'sidebar-nav-active' :'').'" href="acp.php?tn=system&sub=labels">'.$icon['tags'].' '.$lang['labels'].'</a></li>';
 echo '<li><a class="sidebar-nav '.($sub == "categories" ? 'sidebar-nav-active' :'').'" href="acp.php?tn=system&sub=categories">'.$icon['bookmark'].' '.$lang['categories'].'</a></li>';
 
-echo '<li class="mt-2"><a class="sidebar-nav '.($sub == "posts" ? 'sidebar-nav-active' :'').'" href="acp.php?tn=system&sub=posts">'.$icon['pencil_ruler'].' '.$lang['tn_posts'].'</a></li>';
+echo '<li class="mt-2"><a class="sidebar-nav '.($sub == "posts" ? 'sidebar-nav-active' :'').'" href="acp.php?tn=system&sub=posts">'.$icon['file_earmark_post'].' '.$lang['tn_posts'].'</a></li>';
 echo '<li class=""><a class="sidebar-nav '.($sub == "comments" ? 'sidebar-nav-active' :'').'" href="acp.php?tn=system&sub=comments">'.$icon['comments'].' '.$lang['tn_comments'].'</a></li>';
 
 echo '<li class="mt-2"><a class="sidebar-nav '.($sub == "shop" ? 'sidebar-nav-active' :'').'" href="acp.php?tn=system&sub=shop">'.$icon['shopping_basket'].' '.$lang['tn_shop'].'</a></li>';
