e2aaf25a13d847ae385b72631bff443410e2a42d
Patrick <pk@konstand.de>
2022-05-13 09:33:53 +0200

    remove events from section reactions

--- acp/core/inc.reactions.php
+++ acp/core/inc.reactions.php
@@ -2,29 +2,16 @@
 //prohibit unauthorized access
 require 'core/access.php';
 
-
 switch ($sub) {
 
-	case "comments":
-		$subinc = "reactions.comments";
-		break;
-		
-	case "votings":
+    case "votings":
 		$subinc = "reactions.votings";
 		break;
-		
-	case "events":
-		$subinc = "reactions.events";
-		break;
-		
-	case "orders":
-		$subinc = "reactions.orders";
-		break;
-				
-	default:
+
+    case "comments":
+    default:
 		$subinc = "reactions.comments";
 		break;
-
 }
 
 include $subinc.'.php';
